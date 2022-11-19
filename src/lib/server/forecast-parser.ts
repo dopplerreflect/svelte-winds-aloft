9bf0948245f640e4c49f463da3dbcf33e5038a28
David Rose <doppler@gmail.com>
2022-11-18 20:07:54 -0500

    coerce all numbers to Number and divide temp vals by 10

--- src/lib/server/forecast-parser.ts
+++ src/lib/server/forecast-parser.ts
@@ -4,8 +4,10 @@ export const parseForecastText = (text: string) => {
 			forecastText.split('\n').map((t) => t.replace(/^[ ]+/, ''));
 
 		const forecast = rest.map((t) => {
-			const [type, pressure, height, temp, dewpt, direction, speed] = t.split(/[\s]+/);
-			return { height, temp, dewpt, direction, speed };
+			const [type, pressure, height, temp, dewpt, direction, speed] = t
+				.split(/[\s]+/)
+				.map((v) => Number(v));
+			return { height, temp: temp / 10, dewpt: dewpt / 10, direction, speed };
 		});
 		return { header, info, capecin, sid, sounding, sid_other, surface, mandatory, forecast };
 	});
