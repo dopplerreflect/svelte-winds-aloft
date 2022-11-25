a610293b31443eeb872243f4a23fcaabaa4d77e7
David Rose <doppler@gmail.com>
2022-11-25 01:42:45 -0500

    layout tweaks

--- src/routes/[latlon]/+page.svelte
+++ src/routes/[latlon]/+page.svelte
@@ -24,61 +24,73 @@
 	<label for="filter16k">Filter 16k</label>
 	<input id="filter16k" type="checkbox" bind:checked={filter16k} />
 </header>
-<div>Ground elevation: {useFeet ? metersToFeet(data.alt) : data.alt} {heightLabel}</div>
+<div>Ground elevation: {useFeet ? metersToFeet(data.alt) : data.alt}</div>
 <div class="grid-container outer">
 	{#each data.forecasts as forecast}
 		<div class="forecast">
-			<div>{forecast.info.month} {forecast.info.day} {forecast.info.hour}:00 UTC</div>
-			<div>
-				Local time: {toLocalTime(
+			<div class="datetime">
+				{toLocalTime(
 					forecast.info.year,
 					forecast.info.month,
 					forecast.info.day,
 					forecast.info.hour
 				)}
 			</div>
-			<div class="grid-container inner">
-				{#each forecast.soundings.filter((s) => (filter16k ? s.height < 4877 : s)) as sounding}
-					<div>
+			<div class="capecin">CAPE: {forecast.cape} CIN: {forecast.cin}</div>
+			{#each forecast.soundings.filter((s) => (filter16k ? s.height < 4877 : s)) as sounding}
+				<div class="grid-container inner">
+					<div class="height">
 						{useFeet ? metersToFeet(sounding.height - data.alt) : sounding.height - data.alt}
 						{heightLabel}
 					</div>
-					<div>
+					<div class="direction">
 						{sounding.direction}°
 						<div class="arrow" style="transform:rotate({sounding.direction}deg)">ᐁ</div>
 					</div>
-					<div>{useMph ? knotsToMph(sounding.speed) : sounding.speed} {speedLabel}</div>
-					<div>
+					<div class="speed">
+						{useMph ? knotsToMph(sounding.speed) : sounding.speed}
+						{speedLabel}
+					</div>
+					<div class="temperature">
 						{Math.round(Number(useFarenheit ? celsiusToFarenheit(sounding.temp) : sounding.temp))}
 						{temperatureLabel}
 					</div>
-				{/each}
-			</div>
+				</div>
+			{/each}
 		</div>
 	{/each}
 </div>
-<div>
-	<code>{JSON.stringify(data.forecasts, null, 2)}</code>
-</div>
 
+<!-- <div>
+	<code>{JSON.stringify(data.forecasts, null, 2)}</code>
+</div> -->
 <style>
 	.grid-container {
 		font-family: 'Courier New', Courier, monospace;
 	}
 	.grid-container.outer {
+		width: 100vw;
 		display: flex;
 	}
 	.grid-container.inner {
 		display: grid;
 		grid-template-columns: repeat(4, 1fr);
+		border-bottom: 1px solid lightgray;
+	}
+	.grid-container.inner div {
+		margin-right: 0.5em;
 	}
 	.forecast {
-		border-right: 1px solid black;
-		padding-right: 1em;
-		margin-right: 1em;
+		border-left: 1px solid lightgray;
 	}
 	.forecast:last-child {
-		border-right: none;
+		border-right: 1px solid lightgray;
+	}
+	.datetime {
+		background-color: lightblue;
+	}
+	.capecin {
+		background-color: lightgreen;
 	}
 	.arrow {
 		display: inline-block;
