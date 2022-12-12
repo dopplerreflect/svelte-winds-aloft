4d318cf14a6440979802512d33ccc882833486d0
David Rose <doppler@gmail.com>
2022-12-12 07:22:35 -0600

    set and retrieve stored map position

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,9 @@
 	let submitButton: HTMLButtonElement;
 	let lat = 33.9769;
 	let lon = -85.1703;
+	let zoom = 10;
 	let status = '';
+
 	$: latlon = [lat, lon].join(',');
 	$: valid = latlon.match(/^[\d]+\.[\d]+[, ]*[-]?[\d]+\.[\d]+[, \d]*/);
 	$: coords = latlon
@@ -24,41 +26,59 @@
 
 	onMount(async () => {
 		if (browser) {
+			/**
+			 * Attempt to get stored position values from localStorage.
+			 * Fallback to browser geolocation.
+			 */
+			try {
+				({ lat, lon, zoom } = JSON.parse(
+					localStorage.getItem('winds-aloft:mapPosition') ||
+						`{"lat":${lat},"lon":${lon},"zoom":${zoom}}`
+				));
+			} catch {
+				navigator.geolocation.getCurrentPosition((p) => {
+					lat = p.coords.latitude;
+					lon = p.coords.longitude;
+					map.setView([lat, lon]);
+					marker.setLatLng({ lat, lng: lon });
+				});
+			}
+
+			/**
+			 * Set up map
+			 */
 			const leaflet = await import('leaflet');
 			let map = leaflet
-				.map('map', { scrollWheelZoom: 'center', touchZoom: 'center', zoomControl: false })
-				.setView([lat, lon], 7);
-
+				.map('map', { scrollWheelZoom: 'center', touchZoom: 'center', zoomControl: true })
+				.setView([lat, lon], zoom);
 			leaflet.control.zoom({ position: 'bottomright' }).addTo(map);
-
 			leaflet
 				.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 					maxZoom: 19,
 					attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 				})
 				.addTo(map);
-
 			let marker = leaflet.marker([lat, lon], { draggable: true }).addTo(map);
 
-			await navigator.geolocation.getCurrentPosition((p) => {
-				lat = p.coords.latitude;
-				lon = p.coords.longitude;
-				map.setView([lat, lon]);
-				marker.setLatLng({ lat, lng: lon });
-			});
-
-			async function resetLatLon() {
+			/**
+			 * Sync our position with map's position
+			 */
+			async function setStoredPositionFromMap() {
 				let latlng = marker.getLatLng();
 				lat = Number(latlng.lat.toFixed(4));
 				lon = Number(latlng.lng.toFixed(4));
+				zoom = map.getZoom();
+				localStorage.setItem('winds-aloft:mapPosition', JSON.stringify({ lat, lon, zoom }));
 			}
 
+			/**
+			 * Add event listeners to map
+			 */
 			map.on('move', (e) => {
 				marker.setLatLng(map.getCenter());
 			});
-
-			map.on('moveend', resetLatLon);
-
+			map.on('moveend', setStoredPositionFromMap);
+			map.on('zoomend', setStoredPositionFromMap);
 			marker.on('click', () => {
 				submitButton.click();
 			});
