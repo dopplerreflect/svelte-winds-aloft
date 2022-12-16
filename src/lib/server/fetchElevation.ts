4811186e088b90e65d5647008a862c416eb5db74
David Rose <doppler@gmail.com>
2022-12-16 11:20:36 -0600

    Add fetchWithTimeout() and don't wait more than 2000ms for nationalmap.gov

--- src/lib/server/fetchElevation.ts
+++ src/lib/server/fetchElevation.ts
@@ -11,7 +11,7 @@ export const fetchElevation = async (lat: string, lon: string) => {
 	const url = `https://nationalmap.gov/epqs/pqs.php?${queryStr}`;
 	console.log(url);
 	try {
-		const response = await fetch(url);
+		const response = await fetchWithTimeout(url, { timeout: 2000 });
 		const json = await response.json();
 		return json.USGS_Elevation_Point_Query_Service.Elevation_Query.Elevation;
 	} catch (e) {
@@ -19,3 +19,15 @@ export const fetchElevation = async (lat: string, lon: string) => {
 		return 0;
 	}
 };
+
+async function fetchWithTimeout(url: string, options = { timeout: 5000 }) {
+	const { timeout = 8000 } = options;
+
+	const controller = new AbortController();
+	const id = setTimeout(() => controller.abort(), timeout);
+
+	const response = await fetch(url, { ...options, signal: controller.signal });
+	clearTimeout(id);
+
+	return response;
+}
